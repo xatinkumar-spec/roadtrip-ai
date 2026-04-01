d074f3653a0ab9df70937d6886ef3baed0dab819
Jatin Kumar Nayak <xatinkumar@gmail.com>
2026-04-01 22:15:45 +0530

    fix allowed hosts

--- roadtrip_ai/settings.py
+++ roadtrip_ai/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["roadtrip-ai-4v82.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
